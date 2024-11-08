3006e0aac5813d61eb9e5669bc3ae9682feee403
Antonio Pérez <jpgtony@gmail.com>
2024-11-08 23:11:04 +0100

    fin clase 55 - se habilitó SSR en el proyecto

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,11 +6,13 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     // provideZoneChangeDetection({ eventCoalescing: true }),
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes),
+    provideClientHydration(),
   ],
 };
