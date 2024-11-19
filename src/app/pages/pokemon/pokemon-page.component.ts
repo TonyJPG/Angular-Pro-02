0fd194b9d17260aadce0d0f223248c621c4974e6
Antonio Pérez <jpgtony@gmail.com>
2024-11-19 01:25:57 +0100

    fin seccion 67 - skeleton loader

--- src/app/pages/pokemon/pokemon-page.component.ts
+++ src/app/pages/pokemon/pokemon-page.component.ts
@@ -1,11 +1,35 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  signal,
+} from '@angular/core';
 import { PokemonListComponent } from '../../pokemon/pokemon-list/pokemon-list.component';
+import { PokemonListSkeletonComponent } from './ui/pokemon-list-skeleton/pokemon-list-skeleton.component';
 
 @Component({
   selector: 'pokemon-page',
   standalone: true,
-  imports: [PokemonListComponent],
+  imports: [PokemonListComponent, PokemonListSkeletonComponent],
   templateUrl: './pokemon-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PokemonPageComponent {}
+export default class PokemonPageComponent implements OnInit {
+  public isLoading = signal(true);
+
+  // private appRef = inject(ApplicationRef);
+  // private $appState = this.appRef.isStable.subscribe((isStable) => {
+  //   console.log('isStable', isStable);
+  // });
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.isLoading.set(false);
+    }, 1500);
+  }
+
+  // ngOnDestroy(): void {
+  //   console.log('ngOnDestroy');
+  //   this.$appState.unsubscribe();
+  // }
+}
--- src/app/pokemon/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon/pokemon-list/pokemon-list.component.ts
@@ -4,13 +4,8 @@ import { PokemonCardComponent } from '../pokemon-card/pokemon-card.component';
 @Component({
   selector: 'pokemon-list',
   standalone: true,
+  imports: [PokemonCardComponent],
   templateUrl: './pokemon-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [PokemonCardComponent],
 })
-export class PokemonListComponent {
-  private _pokemonArray: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-  get pokemonArray(): number[] {
-    return this._pokemonArray;
-  }
-}
+export class PokemonListComponent {}
