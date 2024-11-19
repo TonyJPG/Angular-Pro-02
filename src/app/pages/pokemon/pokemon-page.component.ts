b85a6437b07f9d2c83cf5ade6f39a42dd947c416
Antonio Pérez <jpgtony@gmail.com>
2024-11-19 16:17:35 +0100

    fin clase 68 - peticiones http

--- src/app/pages/pokemon/pokemon-page.component.ts
+++ src/app/pages/pokemon/pokemon-page.component.ts
@@ -1,11 +1,13 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  inject,
   OnInit,
   signal,
 } from '@angular/core';
 import { PokemonListComponent } from '../../pokemon/pokemon-list/pokemon-list.component';
 import { PokemonListSkeletonComponent } from './ui/pokemon-list-skeleton/pokemon-list-skeleton.component';
+import { PokemonService } from '../../pokemon/services/pokemon.service';
 
 @Component({
   selector: 'pokemon-page',
@@ -16,6 +18,7 @@ import { PokemonListSkeletonComponent } from './ui/pokemon-list-skeleton/pokemon
 })
 export default class PokemonPageComponent implements OnInit {
   public isLoading = signal(true);
+  private pokemonService = inject(PokemonService);
 
   // private appRef = inject(ApplicationRef);
   // private $appState = this.appRef.isStable.subscribe((isStable) => {
@@ -23,13 +26,16 @@ export default class PokemonPageComponent implements OnInit {
   // });
 
   ngOnInit(): void {
+    this.loadPokemon(1);
+
     setTimeout(() => {
       this.isLoading.set(false);
     }, 1500);
   }
 
-  // ngOnDestroy(): void {
-  //   console.log('ngOnDestroy');
-  //   this.$appState.unsubscribe();
-  // }
+  public loadPokemon(page: number): void {
+    this.pokemonService.loadPage(page).subscribe(() => {
+      console.log('loadPokemon onInit');
+    });
+  }
 }
